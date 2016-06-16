7991da3892c2e79b1c33c48a4d05d358a1876380
Ben Peddell <klightspeed@killerwolves.net>
2016-06-16 16:00:33 +1000

    Merge uncommitted changes to Run32

--- Run32/Program.cs
+++ Run32/Program.cs
@@ -5,17 +5,43 @@ using System.Text;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Run32
 {
     class Program
     {
+        [STAThread]
         static int Main(string[] args)
         {
             try
             {
-                Assembly asm = Assembly.LoadFile(args[0]);
-                object ret = asm.EntryPoint.Invoke(null, args.Skip(1).ToArray());
+                string program = Path.GetFullPath(args[0]);
+
+                if (program.EndsWith(".vshost.exe"))
+                {
+                    program = program.Substring(0, program.Length - ".vshost.exe".Length) + ".exe";
+                }
+
+                Assembly asm = Assembly.LoadFile(program);
+                MethodInfo entrypoint = asm.EntryPoint;
+                /*
+                MessageBox.Show(
+                    String.Format("{0} {1}.{2}({3});",
+                        entrypoint.ReturnType.ToString(),
+                        entrypoint.ReflectedType.ToString(),
+                        entrypoint.Name,
+                        String.Join(", ",
+                            entrypoint.GetParameters().Select(p => 
+                                (p.IsIn ? (p.IsOut ? "ref" : "") : (p.IsOut ? "out " : "")) +
+                                p.ParameterType.ToString() + " " +
+                                p.Name
+                            ).ToArray()
+                        )
+                    )
+                );
+                 */
+                object ret = entrypoint.Invoke(null, new object[] { args.Skip(1).ToArray() });
                 if (ret is int)
                 {
                     return (int)ret;
